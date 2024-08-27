0146bc4b61e383ce1102ff45dd813238f10c13a9
HeYinKazune <1254462962@qq.com>
2024-08-27 15:38:13 +0900

    Primary Edge Detection Reproduction.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import cv2 as cv
-import numpy as np
 from utils.yamlReader import read_yaml
 import line_drawer
 
 
 # main function
+# use pytest for testing
 
 def main():
     cfg = read_yaml()
-    drawer = line_drawer.LineDrawer(cfg['brush']['radius'], cfg['brush']['b'], cfg['brush']['g'], cfg['brush']['r'], cfg['laplacian_smoothing']['iter'], cfg['laplacian_smoothing']['lambda'])
-    image = cv.imread(cfg['testFile'])
+    drawer = line_drawer.LineDrawer(cfg['brush']['radius'],
+                                    cfg['brush']['b'],
+                                    cfg['brush']['g'],
+                                    cfg['brush']['r'],
+                                    cfg['laplacian_smoothing']['iter'],
+                                    cfg['laplacian_smoothing']['lambda'])
+    cfg_test_file = cfg['testFile']
+    image = cv.imread(cfg_test_file)
     if image is None:
         print("No image")
         exit(4)
-    drawer.bind_image(image).bind_window(cfg['windowName'])
+    drawer.bind_image(image).bind_window(cfg['windowName']).pre_processing()
     cv.imshow(cfg['windowName'], image)
     cv.setMouseCallback("%s" % cfg['windowName'], drawer.draw_line)
     while True:
